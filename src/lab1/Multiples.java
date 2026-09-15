933ff8f733633075db87a7ef59fcf3af93186eb0
JaylenSze <talesoftimepodcast@gmail.com>
2026-09-15 14:49:33 -0400

    did bonus exercise 1

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples(3, 5, 1000);
+        System.out.println(count);
+
+    }
+
+    static int multiples(int n, int a, int b) {
+        int num = 0;
+        for(int i = 1; i < n; i++){
+            if(i % a == 0 || i % b == 0){
+                num += 1;
+            }
+        }
+        return num;
+    }
+}
